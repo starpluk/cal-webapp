e1b01f8de28cf8ec7a5fba9a87f027007c8f2354
VeerapatK <33505653+starpluk@users.noreply.github.com>
2022-06-24 02:46:09 +0700

    Liff

--- js/cal.js
+++ js/cal.js
@@ -72,45 +72,114 @@ function addCommas(nStr){
 ///////// LIFF Function ///////////
 ///////////////////////////////////
 
-// function getContext() {
-//     if (liff.getContext() != null) {
-//       liff.getContext().type
-//       liff.getContext().viewType
-//       liff.getContext().utouId
-//       liff.getContext().roomId
-//       liff.getContext().groupId
-//     }
-//   }
+function createUniversalLink() {
+    const link1 = liff.permanentLink.createUrl()
+    document.getElementById("universalLink1").append(link1)
 
-// async function getUserProfile() {
-//     const profile = await liff.getProfile()
-//     UserID = profile.userId
-//     UserEmail = liff.getDecodedIDToken().email
-//   }
+    liff.permanentLink.setExtraQueryParam('param=9')
+    const link2 = liff.permanentLink.createUrl()
+    document.getElementById("universalLink2").append(link2)
+  }
 
-// async function sendMsg() {
-//     if (liff.getContext().type !== "none") {
-//       await liff.sendMessages([
-//         {
-//           "type": "sticker",
-//           "stickerId": 1,
-//           "packageId": 1
-//         }
-//       ])
-//       alert("Message sent")
-//     }
-//   }
+  async function shareMsg() {
+    await liff.shareTargetPicker([
+      {
+        type: "text",
+        text: "This message was sent by ShareTargetPicker"
+      }
+    ])
+  }
 
-// async function main() {
-//     liff.ready.then(() => {
-//       liff.isLoggedIn()
-//       if (liff.isLoggedIn()) {
-//         getEnvironment()
-//         getUserProfile()
-//         getContext()
-//       } else {
-//         liff.login()
-//       }
-//     })
-//     await liff.init({ liffId: "1657243570-NkrPBqrJ" })
-//   }
+  function logOut() {
+    liff.logout()
+    window.location.reload()
+  }
+
+  function closed() {
+    liff.closeWindow()
+  }
+
+  async function scanCode() {
+    const result = await liff.scanCode()
+    document.getElementById("scanCode").append(result.value)
+  }
+  
+  function openWindow() {
+    liff.openWindow({
+      url: "https://line.me",
+      external: true
+    })
+  }
+
+  async function getFriendship() {
+    const friend = await liff.getFriendship()
+    document.getElementById("friendship").append(friend.friendFlag)
+    if (!friend.friendFlag) {
+      if (confirm("คุณยังไม่ได้เพิ่ม Bot เป็นเพื่อน จะเพิ่มเลยไหม?")) {
+        window.location = "https://line.me/R/ti/p/@YOUR-BOT-ID"
+      }
+    }
+  }
+
+  async function sendMsg() {
+    if (liff.getContext().type !== "none") {
+      await liff.sendMessages([
+        {
+          "type": "sticker",
+          "stickerId": 1,
+          "packageId": 1
+        }
+      ])
+      alert("Message sent")
+    }
+  }
+
+  function getContext() {
+    if (liff.getContext() != null) {
+      document.getElementById("type").append(liff.getContext().type)
+      document.getElementById("viewType").append(liff.getContext().viewType)
+      document.getElementById("utouId").append(liff.getContext().utouId)
+      document.getElementById("roomId").append(liff.getContext().roomId)
+      document.getElementById("groupId").append(liff.getContext().groupId)
+    }
+  }
+
+  async function getUserProfile() {
+    const profile = await liff.getProfile()
+    document.getElementById("pictureUrl").src = profile.pictureUrl
+    document.getElementById("userId").append(profile.userId)
+    document.getElementById("statusMessage").append(profile.statusMessage)
+    document.getElementById("displayName").append(profile.displayName)
+    document.getElementById("decodedIDToken").append(liff.getDecodedIDToken().email)
+  }
+  
+  function getEnvironment() {
+    document.getElementById("os").append(liff.getOS())
+    document.getElementById("language").append(liff.getLanguage())
+    document.getElementById("version").append(liff.getVersion())
+    document.getElementById("accessToken").append(liff.getAccessToken())
+    document.getElementById("isInClient").append(liff.isInClient())
+    if (liff.isInClient()) {
+      document.getElementById("btnLogOut").style.display = "none"
+    } else {
+      document.getElementById("btnMsg").style.display = "none"
+      document.getElementById("btnScanCode").style.display = "none"
+      document.getElementById("btnClose").style.display = "none"
+    }
+  }
+
+  async function main() {
+    liff.ready.then(() => {
+      document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+      if (liff.isLoggedIn()) {
+        getEnvironment()
+        getUserProfile()
+        getContext()
+        getFriendship()
+        createUniversalLink()
+      } else {
+        liff.login()
+      }
+    })
+    await liff.init({ liffId: "1657243570-NkrPBqrJ" })
+  }
